11194b997820ac215ad5c74963403c2f2d31b088
Riccardo Colombo <riccardo.colombo@ext.wefox.com>
2025-06-03 09:37:59 +0200

    init tailwind4 custom classes

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -124,7 +124,7 @@ export default function LoginForm() {
         <button
           type="submit"
           disabled={isSubmitting}
-          className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50"
+          className="w-full bg-primary-950 dark:bg-primary-100 text-primary-50 dark:text-primary-950 py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50"
         >
           {isSubmitting ? 'Check...' : 'Login'}
         </button>
